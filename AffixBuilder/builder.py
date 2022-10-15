a50a438acb830b0390d499accc9d8c8cdee7bf7b
keith.dyer <keith.dyer@shiftboard.com>
2022-10-15 01:33:09 -0500

    adding in the python files

---
+++ AffixBuilder/builder.py
@@ -0,0 +1,222 @@
+import pandas as pd
+import copy
+
+df = pd.read_csv('affix_inputs.txt', delimiter='\t')
+records = df.to_dict(orient='records')
+
+lvlbreaks = [1] + list(range(5, 70, 5)) + list(range(70, 90, 2)) + list(range(90, 100, 1))
+
+grouping_keys = [
+    'Name',
+    'version',
+    'spawnable',
+    'rare',
+    'level',
+    'maxlevel',
+    'levelreq',
+    'classspecific',
+    'class',
+    'classlevelreq',
+    'frequency',
+    'group',
+    'mod1code',
+    'mod1param',
+    'mod1min',
+    'mod1max',
+    "mod2code",
+    "mod2param",
+    "mod2min",
+    "mod2max",
+    "mod3code",
+    "mod3param",
+    "mod3min",
+    "mod3max",
+    "transformcolor"
+]
+
+addon_columns = [
+    "itype6",
+    "itype7",
+    "etype1",
+    "etype2",
+    "etype3",
+    "etype4",
+    "etype5",
+    "multiply",
+    "add"
+]
+
+template_row = {
+    "Name"  : "emptyString",
+    "version"  : 100,
+    "spawnable"  : 1,
+    "rare"  : 1,
+    "level"  : "",
+    "maxlevel"  : "",
+    "levelreq"  : "",
+    "classspecific"  : "",
+    "class"  : "",
+    "classlevelreq"  : "",
+    "frequency"  : 1,
+    "group"  : "",
+    "mod1code"  : "",
+    "mod1param"  : "",
+    "mod1min"  : "",
+    "mod1max"  : "",
+    "mod2code"  : "",
+    "mod2param"  : "",
+    "mod2min"  : "",
+    "mod2max"  : "",
+    "mod3code"  : "",
+    "mod3param"  : "",
+    "mod3min"  : "",
+    "mod3max"  : "",
+    "transformcolor"  : "",
+    "itype1"  : "",
+    "itype2"  : "",
+    "itype3"  : "",
+    "itype4"  : "",
+    "itype5"  : "",
+    "itype6"  : "",
+    "itype7"  : "",
+    "etype1"  : "",
+    "etype2"  : "",
+    "etype3"  : "",
+    "etype4"  : "",
+    "etype5"  : "",
+    "multiply"  : 0,
+    "add"  : 0
+}
+
+def iteratevalue(prev, range_key, value_key, scaler_key, r):
+    if prev:
+        mid = (prev[1] + prev[0]) / 2
+        rng = prev[1] - prev[0]
+        scaler = (1 + int(r[scaler_key]) / 100)
+        v = (mid, mid + rng * scaler) 
+    else:
+        o = r[range_key] / 2
+        v = (r[value_key] - o, r[value_key] + o)
+    return v
+
+def genrow(lvl, grp, vals, r, itype, name):
+    row = copy.copy(template_row)
+    row["Name"] = row["Name"]
+    row["level"] = lvl
+    row["group"] = grp
+    row["mod1code"] = r['code']
+    row["mod1min"] = int(vals[0]) 
+    row["mod1max"] = int(vals[1])
+    if itype == 'p':
+        row["itype1"] = r['primary']
+    elif itype == 's':
+        row["itype1"] = 'armo' if r['primary'] == 'weap' else 'weap'
+    elif itype == 'j':
+        row['itype1'] = 'ring'
+        row['itype2'] = 'amul'
+    elif itype == 'sc':
+        row['itype1'] = 'jewl'
+    return row
+
+
+files = ['prefix', 'suffix']
+grps = [1, 100]
+
+class NamedLookup():
+    def __init__(self):
+        self.namedLookups = {}
+        self.keybase = 'j'
+        self.keyc = 0
+        
+    def getNamedLookup(self, name):
+        if name not in self.namedLookups:
+            self.namedLookups[name] = f'{self.keybase}{self.keyc:03d}'
+            self.keyc += 1
+        return self.namedLookups[name]
+
+def compressDF(df, grouping_keys, addon_columns):
+    compressionDict = {}
+    keys = df.groupby(grouping_keys)
+
+    items = keys['itype1'].apply(lambda x: [a for a in x if a != '']).reset_index(name='itype1')
+    items['itype2'] = keys['itype2'].apply(lambda x: [a for a in x if a != '']).tolist()
+    items['items'] = items['itype1'] + items['itype2']
+    itypes = pd.DataFrame(items['items'].tolist(), index= items.index, columns=['itype1', 'itype2', 'itype3', 'itype4', 'itype5'])
+
+    compressed = pd.DataFrame(keys.groups.keys(), columns = grouping_keys)
+    compressed = compressed.join(itypes)
+    for k in addon_columns:
+        if k == 'multiply' or k == 'add':
+            compressed[k] = 0
+        else:
+            compressed[k] = ''
+
+    compressed = compressed.sort_values(['mod1code', 'level'])
+    return compressed
+
+lookup = NamedLookup()
+for i in range(2):
+    f = files[i]
+    grp = grps[i]
+    results = []
+
+    for r in records:
+        grp += 1
+        prev = { 'p' : None, 's' : None, 'j' : None, 'sc' : None}
+
+        for idx, lvl in enumerate(lvlbreaks):
+            if lvl < r['minlvl']:
+                continue
+            else:
+                item_types = ['p', 's', 'j', 'sc']
+                for i in item_types:
+                    val = iteratevalue(prev[i], f'{i}range', f'{i}value', 'lvlscalefactor', r)
+                    
+                    # Skip if value and range are 0
+                    if val[0] == val[1] and val[0] == 0:
+                        continue
+
+                    # If range == 0, only run once
+                    if (val[1] - val[0]) == 0:
+                        if prev[i] is not None:
+                            continue
+
+                    name = r['prefix_name'] if f == 'prefix' else r['suffix_name']
+                    key = lookup.getNamedLookup(name)
+                    row = genrow(lvl, grp, val, r, i, key)
+
+                    if r['lvlscalefactor'] != 'static':
+                        row['maxlevel'] = lvlbreaks[idx+5] if idx+5 < len(lvlbreaks) else lvlbreaks[-1]
+
+                    results += [row]
+                    prev[i] = val
+
+                if r['lvlscalefactor'] == 'static':
+                    break
+
+    df = pd.DataFrame(results)
+    df = compressDF(df, grouping_keys, addon_columns)
+    df.to_csv(f'magic{f}.txt', index=False, sep='\t')
+
+
+
+
+# stringIndexStart = 27368
+# string_json_template = {
+#     "id": stringIndexStart,
+#     "Key": "emptyString",
+#     "enUS": ""
+# }
+
+# newStrings = []
+# for idx, s in enumerate(lookup.namedLookups):
+#     r = copy.copy(string_json_template)
+#     r['id'] = stringIndexStart + idx
+#     r["Key"] = lookup.namedLookups[s]
+#     r["enUS"] = s if s != 'emptyString' else ''
+#     newStrings.append(r)
+
+# with open('item-nameaffixes.json', 'w') as ofile:
+#     import json
+#     ofile.writelines(json.dumps(newStrings, indent=4))
+
---
+++ UniqueBuilder/builder.py
@@ -0,0 +1,299 @@
+import pandas as pd
+import copy
+from collections import deque
+import random
+import json
+import numpy as np
+from copy import copy
+import math
+
+affixes = pd.read_csv('unique_inputs.txt', delimiter='\t')
+weapons = pd.read_csv('weapons.txt', delimiter='\t')
+armor = pd.read_csv('armor.txt', delimiter='\t')
+jewelry = pd.read_csv('jewelry.txt', delimiter='\t')
+
+weapons = weapons.query('spawnable == 1')
+weapons = weapons.query('type != "tpot"')
+
+armor = armor.query('spawnable == 1')
+jewelry = jewelry.query('spawnable == 1')
+
+weapons['itemclass'] = 'weap'
+armor['itemclass'] = 'armo'
+
+magic_weapon_types = set(['wand', 'staf', 'orb'])
+
+w = list(weapons[['code', 'level', 'itemclass', 'type']].values)
+a = list(armor[['code', 'level', 'itemclass', 'type']].values)
+j = list(jewelry[['code', 'level', 'itemclass', 'type']].values)
+
+q = deque(w + a + j)
+
+# Load template file
+with open('uniqueitemtemplate.json', 'r') as tfile:
+    template = json.load(tfile)
+
+uniques = []
+
+def build_property_search_tree(affixes):
+    root = {}
+    minlvls = set(affixes['MinLvl'].values)
+    for l in minlvls:
+        itypeTable = affixes.query(f'MinLvl <= {l}')
+        itypes = itypeTable['ItemType'].str.split(',')
+        itypeset = set()
+        for i in itypes:
+            itypeset = itypeset.union(set([x.strip() for x in i]))
+
+        root[l] = {}
+        for i in itypeset:
+            if i == 'shie':
+                elemTypeTable = itypeTable[itypeTable['ItemType'].str.contains(i) | itypeTable['ItemType'].str.contains('armo')]
+            else:
+                elemTypeTable = itypeTable[itypeTable['ItemType'].str.contains(i)]
+
+            elemType = set(elemTypeTable['Type'].values) - set(['all', 'magic'])
+            root[l][i] = {}
+            for e in elemType:
+                if e in ['fire','lightning','poison','cold']:
+                    hlevelTable = elemTypeTable[(elemTypeTable['Type'] == e) | (elemTypeTable['Type'] == 'magic')  | (elemTypeTable['Type'] == 'all')]
+                else:
+                    hlevelTable = elemTypeTable[(elemTypeTable['Type'] == e) | (elemTypeTable['Type'] == 'all')]
+                h = [1,2,3]
+                root[l][i][e] = {}
+                for hlevel in h:
+                    hTable = hlevelTable[hlevelTable['Hierarchy'] == hlevel]
+                    
+                    propCodes = list(set(hTable['Property'].values))
+
+                    # double sample elemental prop codes
+                    propCodes = propCodes + list(set(hTable[(hTable['Type'] == e)]['Property'].values)) 
+
+                    # double sample prop codes with an exact item type match
+                    propCodes = propCodes + list(set(hTable[(hTable['ItemType'] == i)]['Property'].values)) 
+
+                    prop = hTable
+                    root[l][i][e][hlevel] = { 'prop' : prop, 'propCodes' : propCodes }
+    
+    return root
+
+root = build_property_search_tree(affixes)
+
+def search_property(root, lvl, iclass, itype, etype, hlevel, used_prop_codes):
+    # level filter
+    minlvls = list(root.keys())
+    minlvls = sorted(minlvls, reverse=True)
+    for m in minlvls:
+        if m > lvl:
+            continue
+        else:
+            break
+    mlvl = m
+
+    def query(root, iclass, itype, etype, hlevel):
+        try:
+            r1 = root[itype]
+        except KeyError:
+            r1 = root[iclass]
+        
+        r2 = r1[etype]
+
+        try:
+            r3 = r2[hlevel]
+        except KeyError:
+            if hlevel < 3:
+                return query(root, iclass, itype, etype, hlevel+1)
+            else:
+                raise
+
+        # select
+        skills = set(['bar', 'ama', 'nec', 'sor', 'allskills', 'ass', 'dru', 'pal'])
+        used_prop_set = set(used_prop_codes)
+        skillFound = len(skills.intersection(used_prop_set)) > 0
+        prop_codes = []
+        for propertyCode in r3['propCodes']:
+            if skillFound:
+                if propertyCode in skills:
+                    continue
+            else:
+                # Don't add class non assassin class skills to assassin weapons
+                if itype in ['h2h', 'h2h2'] and propertyCode in skills and propertyCode != 'ass':
+                    continue
+
+                # Don't add class non amazon class skills to amazon weapons
+                if itype in ['abow', 'ajav', 'aspe'] and propertyCode in skills and propertyCode != 'ama':
+                    continue
+
+                # Don't add class non druid class skills to druid pelts
+                if itype in ['pelt'] and propertyCode in skills and propertyCode != 'dru':
+                    continue
+                
+                # Don't add class non barb class skills to barb helms
+                if itype in ['phlm'] and propertyCode in skills and propertyCode != 'bar':
+                    continue
+                
+                # Don't add class non pally class skills to pally shields
+                if itype in ['ashd'] and propertyCode in skills and propertyCode != 'pal':
+                    continue
+
+                # Don't add class non necro class skills to necro heads
+                if itype in ['head'] and propertyCode in skills and propertyCode != 'nec':
+                    continue
+
+
+            if propertyCode not in used_prop_set:
+                prop_codes.append(propertyCode)
+
+        if len(prop_codes) == 0:
+            return query(root, iclass, itype, etype, ((hlevel+1) % 3) + 1)
+
+        pc = random.sample(prop_codes, 1)[0]
+        p = r3['prop'].query(f'Property == "{pc}"').sample()
+        return p
+
+    return query(root[mlvl], iclass, itype, etype, hlevel)
+
+
+idx = 0
+items = []
+names = []
+while q:
+    code, level, iclass, itype = q.popleft()
+
+    # Randomly uplevel the item
+    level = level * (random.random() / 3 + 0.9)
+    level = int(level)
+
+    # Sanitize level 0 -> level 1
+    level = max(level, 1)
+    # Sanitize level > 100 -> level 99
+    level = min(level, 99)
+
+    print(code, level, iclass, itype)
+
+    # Extra chance to generate another unique of this type (carries forward upleveling)
+    if random.randint(0, 2) == 0:
+        q.append([code, level, iclass, itype])
+
+    propertyCodes = []
+    properties = []
+
+    # Number of properties
+    minprop, maxprop = int(np.log(level) * 1.5), int(np.log(level) * 3)
+    minprop, maxprop = max(minprop, 3), max(min(maxprop, 12), 3)
+    numprop = random.randint(minprop, maxprop)
+
+    # Select elemental type
+    if itype in magic_weapon_types:
+        etype = random.sample(['cold', 'lightning', 'fire', 'poison'], 1)[0]
+    else:
+        etype = random.sample(['cold', 'lightning', 'fire', 'poison', 'normal'], 1)[0]
+    
+
+    names.append(f'jamellasunique{idx+407}')
+    item = copy(template)
+    item['index'] = f'jamellasunique{idx+407}'
+    item['*ID'] = idx+407
+    item['lvl'] = int(level)
+    item['code'] = code
+
+    c = 0
+    hlevel = 1
+    while c < numprop:
+        prop = search_property(root, level, iclass, itype, etype, hlevel, propertyCodes)
+        propertyCodes.append(prop['Property'].values[0])
+        properties.append(prop)
+
+        if hlevel == 1:
+            hlevel = random.randint(1, 2)
+        elif hlevel == 2:
+            hlevel = random.sample([2, 2, 3], 1)[0]
+        elif hlevel == 3:
+            hlevel = random.sample([3, 3, 3, 1], 1)[0]
+
+
+        param = prop['Param'].values[0]
+        if type(param) == type('str') and param.isnumeric() == False:
+            param = random.sample(param.split(','), 1)[0]
+        
+        func = prop['LvlFunc'].values[0]
+        minval = prop['MinValue'].values[0]
+        maxval = prop['MaxValue'].values[0]
+
+        try:
+            if func != '':
+
+                if func == 'linear':
+                    minval = str(minval).replace('x', str(level))
+                    maxval = str(maxval).replace('x', str(level))       
+                    minval, maxval = math.floor(eval(minval)), math.ceil(eval(maxval))
+                elif func == 'ln':
+                    x = np.log(level)
+
+                    minval = str(minval).replace('x', str(x))
+                    maxval = str(maxval).replace('x', str(x))   
+                    minval, maxval = math.floor(eval(minval)), math.ceil(eval(maxval))
+
+                minval, maxval = float(minval), float(maxval)
+        except SyntaxError:
+            print('ERROR')
+            print(prop, func, minval, maxval)
+            raise
+        except ValueError:
+            print('ERROR')
+            print(prop, func, minval, maxval)
+            raise
+
+        maxval = max(maxval, minval)
+        item[f'prop{c+1}'] = prop['Property'].values[0]
+        item[f'par{c+1}'] = '' if pd.isna(param) else param  
+        item[f'min{c+1}'] = int(minval)
+        item[f'max{c+1}'] = int(maxval)
+
+        c += 1
+
+    idx += 1
+    items.append(item)
+    
+df = pd.DataFrame(items)
+df.to_csv('uniqueitems.txt', index=False, sep='\t')
+
+newnames = pd.read_csv('names.txt', delimiter = '\t', header=None)
+newnames = set(newnames[0].values)
+components = set()
+for n in newnames:
+    parts = n.split(' ')
+    components = components.union(set(parts))
+
+first = []
+second = []
+for k in components:
+    if k in ['of', 'the']:
+        continue
+    first.append(k)
+    if '\'' not in k and 'ing' not in k:
+        second.append(k)
+
+
+id = 273740
+jsons = []
+for n in names:
+    while True:
+        f, s, t = random.sample(first, 1)[0], random.sample(second, 1)[0], random.sample(second, 1)[0]
+        numparts = random.randint(1, 3)
+        newname = s if numparts == 1 else ' '.join([f, s]) if numparts == 2 else ' '.join([f, s, t])
+        if newname not in newnames:
+            newnames.add(newname)
+            break
+    
+    jsons.append(
+        {
+            "id": id,
+            "Key": n,
+            "enUS": newname
+        }
+    )
+    id += 1
+
+with open('names.json', 'w') as ofile:
+    json.dump(jsons, ofile, indent=4)
